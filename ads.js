7b0c9893c7b6761e908d459bbf2140e773c8c272
elmadkouryassin <41273322+elmadkouryassin@users.noreply.github.com>
2018-07-16 19:53:48 +0100

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,25 @@
+<SCRIPT LANGUAGE="Javascript">
+function banner() {};
+banner = new banner();
+number = 0;
+banner[number++] = "<a href='https://codecanyon.net/item/gwd-travel-banner-002/13266841' target='_blank'><img src='https://s3.envato.com/files/153767143/PREVIEW%20IMAGES/728x90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://www.lucidpress.com/pages/templates/banners' target='_blank'><img src='https://d2slcw3kip6qmk.cloudfront.net/marketing/press/images/template-gallery/banner-promotion-728x90.jpeg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://zoogvpn.com/affiliates/' target='_blank'><img src='https://zoogvpn.com/images/affiliates/728x90.png' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://codecanyon.net/item/gwd-play-school-ad-banner-001/13358952' target='_blank'><img src='https://s3.envato.com/files/154677646/PREVIEW%20IMAGES/728x90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://graphicriver.net/item/corporate-banner-ad-set/5503792' target='_blank'><img src='https://s3.envato.com/files/65500288/Orange%20Banner%20ad%20Design%20728x90.png' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='http://www.kingmusic.tk/2017/04/amso-ipseite.html' target='_blank'><img src='https://www.madamasr.com/wp-content/uploads/2016/09/mada-banner-728x90-01.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://graphicriver.net/item/hosting-cloud-banner-ad-set/5077523' target='_blank'><img src='https://s3.envato.com/files/60690608/PNG%20-%20Cloud%20Blue/Cloud%20Banner%20ad%20Design%20728x90.png' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://www.webconnection.com/best-performing-ad-banner-sizes-google-network/' target='_blank'><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX0pg8pwBPE9zv1aGzWUKW_WSFiUF9X-uxA2q97UKu6LtCKHiG' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='http://pexelspace.com/record/' target='_blank'><img src='http://pexelspace.com/record/wp-content/uploads/2016/09/test1.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='' target='_blank'><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_XMvLBoKCk2aQCMHtaZ1TE66gnIhgvlBny0aMFPNUvKGdTGtl' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://www.top10hebergeursweb.com/' target='_blank'><img src='https://tpc.googlesyndication.com/simgad/16822120139541901086' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://www.autosaver.com/AffiliateProgram' target='_blank'><img src='https://www.autosaver.com/Content/themes/App_Themes/EDrive/images/728X90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='http://www.lessorarchive.com/' target='_blank'><img src='http://www.lessorarchive.com/images/ad_dwarikesh.gif' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://graphicriver.net/item/flat-color-sale-web-banner-ads/9017723' target='_blank'><img src='https://s3.envato.com/files/106489822/Big_Sale_Ad_Banners_Images/728x90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://graphicriver.net/item/marketing-banner-set/5056359' target='_blank'><img src='https://s3.envato.com/files/60224096/PNG/Marketing%20Banner%20ad%202%20728x90.png' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://graphicriver.net/item/food-or-restaurant-web-banners/5334940' target='_blank'><img src='https://s3.envato.com/files/63501460/Food%20Web%20Banners%20-%20Preview%20Files/Food%20Web%20Banners%20-%20728x90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='https://codecanyon.net/item/real-estate-banner-ads-html5-animated/13286990' target='_blank'><img src='https://s3.envato.com/files/153878742/screenshots/728x90.jpg' border='0' width='100%' height='90'></a>"
+banner[number++] = "<a href='http://voyageri.ru/' target='_blank'><img src='http://voyageri.ru/wp-content/themes/wt_tera/images/header-ad.jpg' border='0' width='100%' height='90'></a>"
+increment = Math.floor(Math.random() * number);
+document.write(banner[increment]);
+</SCRIPT>
